37d9707e5b57a645f6ec0de28f44f0b4fb204ff3
jolker155 <tuananh.ln155@gmail.com>
2023-06-22 02:12:36 +0700

    update

--- src/components/InfiniteList.tsx
+++ src/components/InfiniteList.tsx
@@ -20,7 +20,9 @@ function InfiniteList() {
     rootMargin: "50px 0px",
     // When the last item comes to the viewport
     onEnter: () => {
-      handleClick();
+      if(loadMore) {
+        handleClick();
+      }
     },
   });
 
